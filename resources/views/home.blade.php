a9ab0ca34efc7d124eda6d1ac450998e41d5af90
Auriol <aurioledemessi7@gmail.com>
2024-06-19 21:01:48 +0100

    media library state 3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,88 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
-                    {{ __('You are logged in!') }}
-                </div>
+<div class="Content">
+                    
+<main class="SingleArtists">
+    <section class="FooterTeaser">
+        <div class="Teasers items--3">
+            <div class="Teasers-item">
+                <a class="/posts" href="rosyne.html">
+                    <figure class="Teasers-item-image Image" >
+                        <img src="images/rosyne.jpg" class="Teasers-item-image-img " width="1707" height="2560"  data-sizes="auto" data-optimumx="2"/>
+                    </figure>
+                    <div class="Teasers-item-info">
+                        <span class="Teasers-item-cta">OUR ARTWORKS</span>
+                    </div>
+                </a>
             </div>
+            <div class="Teasers-item">
+                <a class="" href="/exhibitions">
+                    <figure class="Teasers-item-image Image" >
+                        <img src="images/men1.jpg" class="Teasers-item-image-img " width="1707" height="2560"  data-sizes="auto" data-optimumx="2"/>
+                    </figure>
+                    <div class="Teasers-item-info">
+                        <span class="Teasers-item-cta">EXHIBITIONS</span>
+                    </div>
+                </a>
+            </div>
+            <div class="Teasers-item">
+                <a class="" href="/contact">
+                    <figure class="Teasers-item-image Image" >
+                        <img src="images/vogue.jpg" class="Teasers-item-image-img " width="1707" height="2560"  data-sizes="auto" data-optimumx="2"/>
+                    </figure>
+                    <div class="Teasers-item-info">
+                        <span class="Teasers-item-cta">CONTACT US</span>
+                    </div>
+                </a>
+            </div>
+            <div class="Teasers-item">
+                                    <a class="" href="/posts/create">
+                                        <figure class="Teasers-item-image Image" >
+                                            <img src="images/11.jpg" class="Teasers-item-image-img " width="1707" height="2560"  data-sizes="auto" data-optimumx="2"/>
+                                        </figure>
+                                        <div class="Teasers-item-info">
+                                            <span class="Teasers-item-cta">ADD NEW PROJECT</span>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="Teasers-item">
+                                    <a class="" href="../shows.html">
+                                        <figure class="Teasers-item-image Image" >
+                                            <img src="images/melissa.jpg" class="Teasers-item-image-img " width="1707" height="2560"  data-sizes="auto" data-optimumx="2"/>
+                                        </figure>
+                                        <div class="Teasers-item-info">
+                                            <span class="Teasers-item-cta">ADD NEW TEAM MEMBER</span>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="Teasers-item">
+                                    <a class="" href="../shows.html">
+                                        <figure class="Teasers-item-image Image" >
+                                            <img src="images/ateliervo.jpg" class="Teasers-item-image-img " width="1707" height="2560"  data-sizes="auto" data-optimumx="2"/>
+                                        </figure>
+                                        <div class="Teasers-item-info">
+                                            <span class="Teasers-item-cta">INTERACTIONS</span>
+                                        </div>
+                                    </a>
+                                </div>
+            
+            
+            
+
         </div>
-    </div>
+        </section>
+            
+
+
+
+
+</main>
 </div>
+
+</div>
+
+
 @endsection
